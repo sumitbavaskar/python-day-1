454938e9046d5d209854745a88113b8d1bb5ac3d
sumit <141015819+sumitbavaskar@users.noreply.github.com>
2025-05-25 23:12:08 +0530

    .

---
+++ problem_1.py
@@ -0,0 +1,15 @@
+# Write a  program to print the twinkle twinkle little star program
+# print('''
+
+# Twinkle twinkle little star ,
+# How i wonder what you are,
+# look above sky so high,
+# like a diamond in the sky. 
+
+# ''')
+
+
+import os
+directory_path = "/"
+contents= os.listdir(directory_path)
+print(contents)
